7ff287a3c5e76559a01e92828da93550ad782c77
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-10-21 16:20:03 +0200

    COMPLETAMENTO HTML, INSERIMENTO GENERA BOTTONE IN JS

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// ********************************************************
+// BIGLIETTO TRENO
+// ********************************************************
+
+// REFERENZA ELEMENTI PRINCIPALI DOM
+
+// Bottoni genera / annulla
+var bottoneGenera = document.getElementById('btn-genera-biglietto');
+var bottoneAnnulla = document.getElementById('btn-annulla-biglietto');
+
+// section id="biglietto"
+var containerBiglietto = document.getElementById('biglietto');
+
+// EVENTI DINAMICI
+
+// Calcolo e generazione dinamica biglietto quando l'utente dopo aver inserito i dati clicca sul relativo bottone
+bottoneGenera.addEventListener('click', function(){
+console.log('click');
+
+  
+});
