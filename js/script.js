238aebef6815392c0f25a51f87dacfdc4fa79e0d
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-10-21 17:22:47 +0200

    HTML E JS COMPLETO (bug sconti)

--- js/script.js
+++ js/script.js
@@ -24,33 +24,45 @@ var nome = document.getElementById('nome').value;
 console.log(nome)
 
 var kmDaPercorrere = document.getElementById('km').value;
-console.log(km)
+console.log(kmDaPercorrere)
 
 var fasciaEtà = document.getElementById('fascia-età').value;
 console.log(fasciaEtà)
 
 // Ora devo valutare le varie situazioni (km, minorenne, maggiorenne, over65)  calcolando di conseguenza costi e offerta
 var prezzoKm = 0.21;
-var costoBiglietto = prezzoKm + kmDaPercorrere;
+var costoBiglietto = prezzoKm * kmDaPercorrere;
 
 // Setto il mio default, in questo caso senza scontistiche
 var offerta = 'Biglietto Standard';
 
 if (fasciaEtà == 'minorenne') {
-    costoBiglietto * 0.80;
-    var offerta = 'Applicato sconto minorenni del 20%';
+    costoBiglietto -= costoBiglietto * 0.2;
+    offerta = 'Applicato sconto minorenni del 20%';
 }
 
 else if (fasciaEtà == 'over65') {
-    costoBiglietto * 0.60;
-    var offerta = 'Applicato sconto over65 del 40%';
+    costoBiglietto -= costoBiglietto * 0.4;
+    offerta = 'Applicato sconto over65 del 40%';
 }
 // Gestione decimali valuta (2 decimali)
 costoBiglietto = costoBiglietto.toFixed(2) + ' $';
 
 // Generazione randomica del numero di carrozza da 1 a 10 
 var numeroCarrozza = Math.floor( Math.random() * 10 ) + 1;
-// Generazione randomica del numero di cambio prenotazione da 9000 a 10000
-var numeroCarrozza = Math.floor( Math.random() * 10 ) + 1;
+// Generazione randomica del numero di cambio prenotazione da 9000 a 10000, va letto come 10000-9000 ci da il risultato del range entro cui stare e il + 9000 indica da dove partire la generazione randomica il tutto ha come risultato 'da 9000 a 10000'
+var numeroCambioPrenotazione = Math.floor( Math.random() *  (10000 - 9000 ) ) + 9000;
+
+// Ora inseriamo i valori ricavati nella span che avevamo precedentemente creato in HTML
+
+document.getElementById('nome-passeggero').innerHTML = nome;
+
+document.getElementById('offerta-applicata').innerHTML = offerta;
+
+document.getElementById('carrozza').innerHTML = numeroCarrozza;
+
+document.getElementById('codice-cambio-prenotazione').innerHTML = numeroCambioPrenotazione;
+
+document.getElementById('costo').innerHTML = costoBiglietto;
 
 });
